410c7431b92aaf1275c38c37ee350f6844af54f4
Lucie Choi <ychoi0407@gmail.com>
2024-12-30 16:45:40 -0800

    Start text-geometry

---
+++ text-geometry/src/World/components/birds/birds.js
@@ -0,0 +1,23 @@
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+
+import { setupModel } from './setupModel.js';
+
+async function loadBirds() {
+  const loader = new GLTFLoader();
+
+  const [parrotData,] = await Promise.all([
+    loader.loadAsync('/assets/models/Parrot.glb'),
+  ]);
+
+  console.log('Squaaawk!', parrotData);
+
+  const parrot = setupModel(parrotData);
+  parrot.position.set(0, 0, 2.5);
+  parrot.scale.set(0.1, -0.1, 0.1);
+
+  return {
+    parrot,
+  };
+}
+
+export { loadBirds };
