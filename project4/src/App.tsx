4967a65e6c16cce132acc3be236ef949bddde8ca
Lucie Choi <ychoi0407@gmail.com>
2025-03-13 15:36:26 -0700

    Added navigation

--- project4/src/App.tsx
+++ project4/src/App.tsx
@@ -1,155 +1,19 @@
-import { useState, useRef, useMemo, useEffect } from 'react'
-import { Canvas, useLoader, invalidate } from '@react-three/fiber'
-import { OrbitControls } from '@react-three/drei'
-import VertexShader from './assets/shaders/kernel/vertex.glsl?raw'
-import FragmentShader from './assets/shaders/kernel/fragment.glsl?raw'
-import './App.css'
-import { TextureLoader, Uniform, Vector2 } from 'three'
-import { Leva, useControls } from 'leva'
-import image1 from './assets/images/me.jpg'
-import ReactMarkdown from 'react-markdown';
-import { MathJaxContext, MathJax } from "better-react-mathjax";
-
-const kernelOptions = [
-  'original',
-  'box blur',
-  'gaussian blur',
-  'sharpen',
-  'sobel vertical',
-  'sobel horizontal',
-  'sobel edge detection'
-];
-
-const kernels = {
-  'original': {
-    kernel: [0, 0, 0, 0, 1, 0, 0, 0, 0],
-    kernelWeight: 1
-  },
-  'box blur': {
-    kernel: [1, 1, 1, 1, 1, 1, 1, 1, 1],
-    kernelWeight: 9
-  },
-  'gaussian blur': {
-    kernel: [
-      1, 2, 1,
-      2, 4, 2,
-      1, 2, 1
-    ],
-    kernelWeight: 16
-  },
-  'sharpen': {
-    kernel: [
-      0, -1, 0,
-      -1, 5, -1,
-      0, -1, 0
-    ],
-    kernelWeight: 1
-  },
-  'sobel vertical': {
-    kernel: [
-      1,  0, -1,
-      2,  0, -2,
-      1,  0, -1
-    ],
-    kernelWeight: 1
-  },
-  'sobel horizontal': {
-    kernel: [
-      1,  2,  1,
-      0,  0,  0,
-     -1, -2, -1
-    ],
-    kernelWeight: 1
-  },
-};
-
-// TODO:
-// 1. Show kernel used
-// 2. Input custom kernel
-const Image = ({kernelType = 'original'}) => {
-  const tx = useLoader(TextureLoader, image1);
-  const uniforms = useRef();
-  uniforms.current ??= {
-    u_texture: { value: tx },
-    u_textureSize: new Uniform(new Vector2(tx.image.width, tx.image.height)),
-    u_kernelWeight: { value: 0 },
-    u_kernel: { value: [0, 0, 0, 0, 0, 0, 0, 0, 0]},
-    u_kernel2: { value: [0, 0, 0, 0, 0, 0, 0, 0, 0]},
-    u_is_sobel_edge: { value: false }
-  }
-  
-  useEffect(() => {
-    console.log("Updating kernel type to " + kernelType)
-    if (kernelType == 'sobel edge detection') {
-      uniforms.current.u_kernel.value = kernels['sobel vertical']['kernel'];
-      uniforms.current.u_kernel2.value = kernels['sobel horizontal']['kernel'];
-      uniforms.current.u_is_sobel_edge.value = true;
-      uniforms.current.u_kernelWeight.value = 1;
-
-    } else {
-      uniforms.current.u_kernel.value = kernels[kernelType]['kernel'];
-      uniforms.current.u_kernelWeight.value = kernels[kernelType]['kernelWeight'];
-      uniforms.current.u_is_sobel_edge.value = false;
-    }
-  }, [kernelType]);
-
-  return (
-    <mesh>
-      <planeGeometry args={[5, 5]}/>
-      <shaderMaterial
-        vertexShader={VertexShader}
-        fragmentShader= {FragmentShader}
-        uniforms={uniforms.current}
-      />
-    </mesh>
-  )
-};
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import KernelPage from "./pages/Kernel";
+import RasterizationPage from "./pages/Rasterization";
 
 function App() {
-  const kernelOption
-    = useControls( { kernel: { value: 'original', options: kernelOptions } });
-
   return (
     <>
-      <MathJaxContext>
-        <div className="header-div">
-          <h1>Image Processing</h1>
-          <h2>{ kernelOption.kernel }</h2>
-          <MathJax>
-            {getMarkdown(kernelOption.kernel)}
-          </MathJax>
-        </div>
-        <div className="canvas-div">
-          <Leva />
-          <Canvas>
-            <Image kernelType={kernelOption.kernel}/>
-            <OrbitControls />
-          </Canvas>
-        </div>
-      </MathJaxContext>
+    <Router>
+        <Routes>
+          <Route path="/" element={<KernelPage />} />
+          <Route path="/rasterization" element={<RasterizationPage />} />
+        </Routes>
+      </Router>
     </>
   )
-}
-
-const getMarkdown = (kernelType = 'original') =>  {
-  if (kernelType == 'sobel edge detection') {
-    const gx = getMatrixMarkdown(kernels['sobel horizontal']['kernel']);
-    const gy = getMatrixMarkdown(kernels['sobel horizontal']['kernel']);
-    const markdown = `\\(\\sqrt{ (${gx} \\times I )^2 + (${gy} \\times I )^2 } \\)`;
-    return markdown;
-  }
-  const matrix = getMatrixMarkdown(kernels[kernelType]['kernel']);
-  const markdown = `\\( ${matrix} \\times I \\)`;
-  return markdown;
-};
-
-const getMatrixMarkdown = (m) => {
-  let matrix = "\\begin{bmatrix} ";
-  matrix += `${m[0]} & ${m[1]} & ${m[2]} \\\\`;
-  matrix += `${m[3]} & ${m[4]} & ${m[5]} \\\\`;
-  matrix += `${m[6]} & ${m[7]} & ${m[8]}`;
-  matrix += " \\end{bmatrix}";
-  return matrix;
 }
 
 export default App
